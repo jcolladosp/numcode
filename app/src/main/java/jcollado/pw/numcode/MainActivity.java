deae83c27499677d2b5ad6aa0880616f72756a62
Jose Collado San Pedro <jocolsan@inf.upv.es>
2019-03-20 21:13:51 +0100

    v 1.4

--- app/src/main/java/jcollado/pw/numcode/MainActivity.java
+++ app/src/main/java/jcollado/pw/numcode/MainActivity.java
@@ -4,31 +4,31 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.text.InputType;
 import android.util.DisplayMetrics;
-
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.pddstudio.urlshortener.URLShortener;
-import com.thefinestartist.finestwebview.FinestWebView;
 
 import java.util.Locale;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsCallback;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.core.content.ContextCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -50,9 +50,8 @@ public class MainActivity extends AppCompatActivity {
     FancyButton buttonContactList;
     String url;
     protected ProgressDialog progressBar;
-
-    boolean noExists = true;
-
+    CustomTabsIntent.Builder builder;
+    CustomTabsIntent customTabsIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,51 +67,60 @@ public class MainActivity extends AppCompatActivity {
         url =  "http://www.numcode.com/" + getLocale();
         progressBar = new ProgressDialog(this);
         progressBar.setIndeterminate(true);
+
+        builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.accent));
+
+        customTabsIntent = builder.build();
+        builder.setShowTitle(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
 
     @OnClick(R.id.settingsButton)
     public void onSettings(){
         String num = Functions.getNumCodePrefs(sharedPref);
-        boolean wrapInScrollView = true;
 
         MaterialDialog dialog =new MaterialDialog.Builder(this)
                 .title(getString(R.string.settings))
-                .customView(R.layout.settings, wrapInScrollView)
+                .customView(R.layout.settings, true)
                 .positiveText(R.string.save)
-                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                        View view = dialog.getCustomView();
-                        RadioGroup buttons = (RadioGroup) view.findViewById(R.id.languageRG);
+                .onPositive((dialog1, which) -> {
+                    View view = dialog1.getCustomView();
+                    RadioGroup buttons = view.findViewById(R.id.languageRG);
 
-                        EditText editText =(EditText) view.findViewById(R.id.numcodeED);
+                    EditText editText =view.findViewById(R.id.numcodeED);
 
-                        int radioButtonID = buttons.getCheckedRadioButtonId();
-                        View radioButton = buttons.findViewById(radioButtonID);
-                        int idx = buttons.indexOfChild(radioButton);
-                        switch (idx) {
-                            case 0:
-                                setLocale("es");
-                                break;
-                            case 1:
-                                setLocale("fr");
-                                break;
-                            case 2:
-                            default:
-                                setLocale("en");
-                                break;
-                        }
-                        Functions.writeNumCodePrefs(sharedPref,editText.getText().toString());
-
+                    int radioButtonID = buttons.getCheckedRadioButtonId();
+                    View radioButton = buttons.findViewById(radioButtonID);
+                    int idx = buttons.indexOfChild(radioButton);
+                    switch (idx) {
+                        case 0:
+                            setLocale("es");
+                            break;
+                        case 1:
+                            setLocale("fr");
+                            break;
+                        case 2:
+                        default:
+                            setLocale("en");
+                            break;
                     }
+                    Functions.writeNumCodePrefs(sharedPref,editText.getText().toString());
+
                 })
                 .show();
 
         View view = dialog.getCustomView();
-        RadioButton spanish = (RadioButton) view.findViewById(R.id.spanishRB);
-        RadioButton english = (RadioButton) view.findViewById(R.id.englishRB);
-        RadioButton french = (RadioButton) view.findViewById(R.id.frenchRB);
-        EditText editText =(EditText) view.findViewById(R.id.numcodeED);
+        RadioButton spanish =  view.findViewById(R.id.spanishRB);
+        RadioButton english =  view.findViewById(R.id.englishRB);
+        RadioButton french =  view.findViewById(R.id.frenchRB);
+        EditText editText = view.findViewById(R.id.numcodeED);
         editText.setText(num);
 
         String locale = getResources().getConfiguration().locale.toString();
@@ -140,13 +148,14 @@ public class MainActivity extends AppCompatActivity {
     }
     @OnClick(R.id.buttonWeb)
     public void onWeb(){
-
-        new FinestWebView.Builder(this).titleDefault(getString(R.string.numcode_button)).titleColor(ContextCompat.getColor(this, R.color.accent)).updateTitleFromHtml(false).show(url);
+        customTabsIntent.launchUrl(this, Uri.parse(url));
     }
     @OnClick(R.id.buttonContactList)
     public void onContactList(){
         String contactListURL = "http://www.numcode.com/fr/index.php?controller=contact_list";
-        new FinestWebView.Builder(this).titleDefault(getString(R.string.contact_list_button)).titleColor(ContextCompat.getColor(this, R.color.accent)).updateTitleFromHtml(false).show(contactListURL);
+        customTabsIntent.launchUrl(this, Uri.parse(contactListURL));
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
     }
 
     @OnClick(R.id.buttonProfile)
@@ -158,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             String url = "http://www.numcode.com/"+ getLocale() + "/recherche?controller=search&orderby=position&orderway=desc&search_query="+numcode+"&submit_search=";
-            new FinestWebView.Builder(this).titleDefault(getString(R.string.numcode_profile)).titleColor(ContextCompat.getColor(this, R.color.accent)).updateTitleFromHtml(false).show(url);
+            customTabsIntent.launchUrl(this, Uri.parse(url));
 
         }
 
@@ -184,37 +193,21 @@ public class MainActivity extends AppCompatActivity {
                 .title(getString(R.string.save_numcode))
                 .content(getString(R.string.numcode_alert_body))
                 .inputType(InputType.TYPE_CLASS_NUMBER )
-
-
                 .negativeText(getString(R.string.register))
-                .input("","", new MaterialDialog.InputCallback() {
-                    @Override
-                    public void onInput(MaterialDialog dialog, CharSequence input) {
-                        // Do something
-                    }
-                })
-                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                .onPositive((dialog, which) -> {
 
 
-                            Functions.writeNumCodePrefs(sharedPref, dialog.getInputEditText().getText().toString());
-                            if (profile)
-                                onProfile();
-                            else {
-                                onShare();
-                            }
+                    assert dialog.getInputEditText() != null;
+                    Functions.writeNumCodePrefs(sharedPref, dialog.getInputEditText().getText().toString());
+                        if (profile)
+                            onProfile();
+                        else {
+                            onShare();
                         }
+                    })
+                .onNegative((dialog, which) -> customTabsIntent.launchUrl(this, Uri.parse(url)));
 
-                })
-                .onNegative(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
 
-                        new FinestWebView.Builder(getApplicationContext()).show(url);
-                    }
-                })
-                .show();
     }
 
     public String getLocale(){
@@ -247,6 +240,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 public void share(String url){
+    progressBar.hide();
     Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
     sharingIntent.setType("text/plain");
     sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.share_body)+" "+url +
@@ -260,4 +254,6 @@ public void share(String url){
         progressBar.setCancelable(false);
         progressBar.show();
     }
+
+
 }
